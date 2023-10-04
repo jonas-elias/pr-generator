c4d379b055078b369c8046a31f259fdb17bd3000
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-04 18:13:39 -0300

    style: code variables

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ console = Console()
 option_style = "bold cyan"
 success_style = "bold green"
 
-arquivo = open(sys.argv[1])
+file = open(sys.argv[1])
+content = file.read()
 
-conteudo = arquivo.read()
+console.print(f'[{option_style}]Carregando...')
 
 response = openai.Completion.create(
   model="gpt-3.5-turbo-instruct",
-  prompt=f"Faça uma descrição em português-br para um pull request do código {conteudo}",
+  prompt=f"Faça uma descrição em português-br para um pull request do código {content}",
   max_tokens=500,
   temperature=0.9
 )
@@ -26,7 +27,6 @@ descricao_pr = response['choices'][0]['text']
 
 command = ['gh', 'pr', 'create', '--title', title_pr, '--body', descricao_pr]
 
-console.print(f'[{option_style}]Carregando...')
 result = subprocess.run(command, capture_output=True, text=True)
 
 if result.returncode != 0:
