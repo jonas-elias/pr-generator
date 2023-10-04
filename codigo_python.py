b59f993add7e9e9572ba38cb451342f022df9ce3
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-04 17:33:14 -0300

    feat: index php

---
+++ codigo_python.py
@@ -0,0 +1,10 @@
+# Solicita ao usuário que insira três números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+# Calcula a média dos números
+media = (numero1 + numero2 + numero3) / 3
+
+# Exibe a média
+print("A média dos números é:", media)
